42be579bc225bf9f20e86391cc5838f215536d05
Dhaval Bhatia <dhavalbhatia@Dhavals-MacBook-Pro.local>
2023-10-02 15:14:22 -0600

    final changes

--- README.md
+++ README.md
@@ -24,3 +24,13 @@ examples thus some of the code is Copyright © 2001-2013 Python
 Software Foundation; All Rights Reserved under the PSF license (GPL
 compatible) http://docs.python.org/2/library/socketserver.html
 
+References
+========================
+1. Format Http response:
+    https://docs.netscaler.com/en-us/citrix-adc/current-release/appexpert/http-callout/http-request-response-notes-format.html
+    https://developer.mozilla.org/en-US/docs/Glossary/Response_header
+    https://stackoverflow.com/questions/6324167/do-browsers-send-r-n-or-n-or-does-it-depend-on-the-browser
+
+2. Content-type info:
+    https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Type
+
--- server.py
+++ server.py
@@ -27,7 +27,6 @@ import os
 
 # try: curl -v -X GET http://127.0.0.1:8080/
 
-UNACCEPTED_METHODS = ['POST', 'PUT', 'DELETE']
 ROOT_PATH = 'www/'
 
 class MyWebServer(socketserver.BaseRequestHandler):
@@ -39,53 +38,55 @@ class MyWebServer(socketserver.BaseRequestHandler):
         self.get_response(self.data)
     
     def get_response(self, data):
-        data = data.decode('utf-8')
-        print("data is", data)
-        
-        # check if there are any unpermitted methods in request
-        if any(method in data for method in UNACCEPTED_METHODS):
-            response = f"HTTP/1.1 405 Method Not Allowed\r\nContent-Type: text/plain;\r\n\r\n405 Try a different method"
-            return self.request.sendall(bytearray(response, 'utf-8'))
-        
-        # parsing get request
-        request_path = data.split(' ')[1]
-        print("request path is", request_path)
-        path = request_path[1:]
-        if ROOT_PATH not in path:
-            path = ROOT_PATH + path
-        print("final path is", path)
-        
-        # handling two mime types
-        content_type = ''
-        if path.endswith('.html'):
-            content_type = 'text/html'
-        elif path.endswith('.css'):
-            content_type = 'text/css'
-        
-        # handle dir with / and without /
-        elif os.path.isdir(path):
-            print(path)
-            path_301 = ''.join(path.split('/')[1:])
-            if not path.endswith('/'):
-                path += '/'
-                response = f"HTTP/1.1 301 Moved Permanantly\r\nLocation:http://127.0.0.1:8080/{path_301}/\r\nContent-Type: text/html;\r\n\r\n301 Not Found"
+        try:
+            data = data.decode('utf-8')
+            request_path = data.split(' ')[1]
+            path = request_path[1:]
+            
+            # check if there are any unpermitted methods in request
+            if not data.startswith('GET'):
+                response = f"HTTP/1.1 405 Method Not Allowed\r\nContent-Type: text/plain;\r\n\r\n405 Try a different method"
+                return self.request.sendall(bytearray(response, 'utf-8'))
+            
+            elif path.startswith('../'):
+                response = f"HTTP/1.1 404 Not FOUND\r\nContent-Type: text/html;\r\n\r\n404 Not Found"
                 self.request.sendall(bytearray(response, 'utf-8'))
                 return
-        
-        try:
-            if os.path.isdir(path):
-                path += 'index.html'
+            # parsing get request and adding 'www' if needed
+            
+            elif ROOT_PATH not in path:
+                path = ROOT_PATH + path
+            
+            # handling two mime types
+            content_type = ''
+            if path.endswith('.html'):
                 content_type = 'text/html'
-            print("path in try", path)
-            file = open(path, "r")
-            file_content = file.read()
-            file.close()
-            response = f"HTTP/1.1 200 OK\r\nContent-Type: {content_type};\r\n\r\n{file_content}"
-        except:
-            response = f"HTTP/1.1 404 Not FOUND\r\nContent-Type: {content_type};\r\n\r\n404 Not Found"
-        self.request.sendall(bytearray(response, 'utf-8'))
+            elif path.endswith('.css'):
+                content_type = 'text/css'
+            
+            # handle dir with / and without /
+            elif os.path.isdir(path):
+                path_301 = ''.join(path.split('/')[1:])
+                if not path.endswith('/'):
+                    path += '/'
+                    response = f"HTTP/1.1 301 Moved Permanantly\r\nLocation:http://127.0.0.1:8080/{path_301}/\r\nContent-Type: text/html;\r\n\r\n301 Not Found"
+                    self.request.sendall(bytearray(response, 'utf-8'))
+                    return
+            
+            try:
+                if os.path.isdir(path):
+                    path += 'index.html'
+                    content_type = 'text/html'
+                file = open(path, "r")
+                file_content = file.read()
+                file.close()
+                response = f"HTTP/1.1 200 OK\r\nContent-Type: {content_type};\r\n\r\n{file_content}"
+            except:
+                response = f"HTTP/1.1 404 Not FOUND\r\nContent-Type: {content_type};\r\n\r\n404 Not Found"
+            self.request.sendall(bytearray(response, 'utf-8'))
 
-        
+        except:
+            print("Unknown request")
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
 
